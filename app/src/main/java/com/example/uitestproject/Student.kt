6312aa8e4c6439873264ae24e7fcb1914cb8bcc5
CN <ckddnjs554@naver.com>
2021-05-20 21:13:35 +0900

    Feat : Student Class Serializable 적용

--- app/src/main/java/com/example/uitestproject/Student.kt
+++ app/src/main/java/com/example/uitestproject/Student.kt
@@ -1,8 +1,10 @@
 package com.example.uitestproject
 
+import java.io.Serializable
+
 class Student(
     var id: Int? = null,
     var name: String? = null,
     var age: Int? = null,
     var intro: String? = null
-)
+): Serializable
